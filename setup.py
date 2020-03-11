9a07ba5688b67f25c64d897cf22b4fd5f90c8f6b
ushills <web@ushills.co.uk>
2020-03-11 15:55:47 +0000

    Added CI to publish to PyPi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="msgtopdf",
-    version="0.0.4",
+    version="0.1.0",
     author="Ian Hill",
     author_email="web@ushills.co.uk",
     description="Convert Outlook msg to PDF",
